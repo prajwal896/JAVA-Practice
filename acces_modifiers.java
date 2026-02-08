137c25c7bfd7a161a373e01d211e452cd5c51fc3
prajwal896 <prajwaladaki999@gmail.com>
2026-02-08 15:11:24 +0530

    acces modifers and constructors and its overloadin too

---
+++ acces_modifiers.java
@@ -0,0 +1,120 @@
+//setting raduis and heihgt of cylinder and calculating surface araea and volume of cylinder
+/*
+public class acces_modifiers{
+static class cylinder{
+private int radius;
+private int height;
+
+public void setradius(int radius){
+    this.radius=radius;
+}
+public int getradius(){
+    return radius;
+}
+
+public void setheight(int height){
+    this.height=height;
+}
+public int getheight(){
+    return height;
+}
+public double volume(){
+    return 3.14*radius*radius*height;
+}
+public double surfacearea(){
+    return 2*3.14*radius*height+2*3.14*radius*radius;
+}
+}
+public static void main(String[] args){
+cylinder cy = new cylinder();
+cy.setheight(5);
+cy.setradius(10);
+System.out.println("height is ="+cy.getheight());
+System.out.println("radius is ="+cy.getradius());
+System.out.println("volume ="+cy.volume());
+System.out.println("surface area ="+cy.surfacearea());
+}
+}
+
+// setting and getting useing constructors for sphere
+
+public class acces_modifiers {
+static class cylinder{
+private int radius;
+private int height;
+    public cylinder(int radius, int height){
+    this.radius =radius;
+    this.height =height;
+    }
+
+    public void setradius(int radius){
+    this.radius=radius;
+    }
+    public int getradius(){
+        return radius;
+    }
+    public void setheight(int height){
+    this.height=height;
+    }
+    public int getheight(){
+        return height;
+    }
+}
+    public static void main(String[] args){
+cylinder cy = new cylinder(5,10);
+System.out.println(cy.getheight());
+    
+System.out.println(cy.getradius());
+}
+
+}
+
+//same for sphere using consturctors
+public class acces_modifiers{
+static class sphere{
+private int radius;
+public sphere(){
+    radius=45;
+}
+    public void setradius(int radius){
+        this.radius=radius;
+    }
+    public int getradius(){
+        return radius;
+    }
+}
+
+public static void main(String[] args){
+sphere sp = new sphere();
+System.out.println(sp.getradius());
+}
+}*/ 
+
+// constructor overloading in rectangle
+/* 
+public class acces_modifiers {
+
+static class rectangle{
+private int length;
+private int breadth;  
+public rectangle(){
+    this.breadth=2;
+    this.length=3;
+}
+public rectangle(int breadth, int length){
+    this.breadth=breadth;
+    this.length=length;
+}
+public int getbreadth(){
+    return breadth;
+}
+public int getlength(){
+    return length;
+}
+}
+public static void main(String[] args){
+rectangle re = new rectangle(45,23);
+System.out.println(re.getbreadth());
+System.out.println(re.getlength());
+}
+}*/
