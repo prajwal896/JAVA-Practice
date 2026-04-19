f8444f228216a9759f5bfb80c9f2e1088fa61135
prajwal896 <prajwaladaki999@gmail.com>
2026-04-19 20:14:03 +0530

    basicpractice q of threads in java

--- p7.java
+++ p7.java
@@ -1,19 +1,31 @@
 class greet extends Thread{
 public void run(){
-    while(true){
+    int i=0;
+    while(i<100){
         System.out.println("\nGood morning");
+        i++;
     }
 }
 }
-class greet extends Thread{
+class greet1 extends Thread{
 public void run(){
-    while(true){
-        System.out.println("\nGood morning");
+    int i=0;
+    while(i<100){
+        System.out.println("\nWelcome");
+        i++;
     }
 }
 }
 public class p7{
     public static void main(String[] args) {
-        
+        greet t1=new greet();
+        greet1 t2 = new greet1();
+    t2.setPriority(Thread.MAX_PRIORITY);
+    t1.setPriority(Thread.MIN_PRIORITY);
+        System.out.println("\npriority  of 1 is " + t1.getPriority());
+        System.out.println("\npriority  of 1 is " + t2.getPriority());
+        t1.start();
+        t2.start(); 
+
     }
 }
